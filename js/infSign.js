b39b6e3d9340f10d172abe1a607a9a413e23f232
Simofy <julsimass@gmail.com>
2018-05-22 16:59:18 +0300

    Minor
    Added function infAnimationToggle() for stoping inf sign animation. Removed unnecessary variables.

--- js/infSign.js
+++ js/infSign.js
@@ -4,12 +4,17 @@ $(document).ready(function(){
     //  Utils.
     //
     //--------------------------------------------------------------------------------------------------------------
+    var animate = true;
+    window.infAnimationToggle = function(){
+        animate = !animate;
+    }
     /**
      * Gets random float.
      * @param {number} min - From.
      * @param {number} max - To.
      * @returns {number}
      */
+
     var randomFloat = function (min, max) {
         return min + (max - min) * Math.random();
     };
@@ -24,16 +29,12 @@ $(document).ready(function(){
         return Math.round(randomFloat(min, max));
     };
     var infSketch = function(){
-        this.d = 8;
-        this.n = 5;
         this.scale = stage.width() / test;
-        this.size = 25;
+        this.size = 40;
         this.colorArray = [];
         this.shapeArray = [];
-        this.step = 0.06;
+        this.step = 0.04;
         this.time = 0.0;
-        this.delay = 10;
-        this.randDigit = 0;
         this.layer = stage.layer();
         //x = cos t , y = sin t * cos t
         for(let i = 0; i < this.size; i++) {
@@ -60,9 +61,11 @@ $(document).ready(function(){
         let b_ = randomInt(0,255);
         this.colorArray.shift();
         this.colorArray.push({r:r_, g:g_, b:b_});
-        let start = getPos(this.time);
+        let start = getPos(this.time + this.step);
         let end = getPos(this.time - (this.size-1) * this.step);
-        for (var a = 0; a < this.size; a++) {
+        //let end = getPos(this.time);
+        //let last = 
+        for (var a = 1; a < this.size; a++) {
             let pos = getPos(this.time - a * this.step);
             let c = this.colorArray[(this.size-1)- a];
             let linePath = this.shapeArray[a];
@@ -72,6 +75,8 @@ $(document).ready(function(){
             linePath.lineTo(start[0]*this.scale + stage.width() / 2, start[1]*this.scale + stage.height() / 2);
             linePath.fill("rgb("+c.r+","+c.g+","+c.b+")");
             linePath.stroke('none');
+            //end = getPos(this.time - (a) * this.step);
+            //start = getPos(this.time - (a+20) * this.step );
             linePath.close();
         }
         stage.resume();
@@ -89,9 +94,18 @@ $(document).ready(function(){
     draw();
     function draw() {
         window.requestAnimationFrame(draw);
-        infSign.draw();
+        if(animate)
+            infSign.draw();
     }
-    window.addEventListener('resize', function(){
-        infSign.windowResized();
-      }, true);
+    // window.addEventListener('resize', function(){
+    //     infSign.windowResized();
+    //   }, true);
+    // window.addEventListener('mousedown', function(){
+    // animate = true;
+    // }, true);
+    // window.addEventListener('mouseup', function(){
+    //     animate = false;
+    //     }, true);
+
+
 });
